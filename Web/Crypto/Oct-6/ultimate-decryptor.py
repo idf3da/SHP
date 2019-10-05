8cc51940ed59e75772cbdcdf5cdf98b3c3931e5e
= <=>
2019-10-05 23:35:12 +0300

    Oct-05

--- Web/Crypto/Oct-6/ultimate-decryptor.py
+++ Web/Crypto/Oct-6/ultimate-decryptor.py
@@ -1,4 +1,12 @@
-class decrypter():
+import random
+
+win1251 = {}
+
+rus = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯфбвгдежзийклмнопрстуфхцчшщъыьэюя'
+for i in range(0, len(rus)):
+    win1251[i + 192] = rus[i]
+
+class decoder():
     def __init__(self):
         self.value = True
 
@@ -23,13 +31,69 @@ class decrypter():
         for i in range(len(figures)):
             result += figures[i] * base ** i
         return result
-    
-a = decrypter()
 
-s = {
-    1:a.nth2dec,
-    2:a.dec2nth
-}
+    def bin2dec(self, n):
+        return int(n, 2)
+
+    def dec2bin(self, n):
+        return bin(int(n))[2:]
+
+    def dec2rus(self, n):
+        if n in win1251.keys():
+            return win1251[n]
+        else:
+            return chr(int(n))
+    def chunks(self, l, n):
+        for i in range(0, len(l), n):
+            yield l[i:i + n]
+
+    
+    def elias_decode(self, s):
+        prefix = int(s[0])
+        s = [int(i) for i in s[1:]]
+        fragments = []
+        start = 0
+        c = 0
+        i = 0
+        while i < len(s):
+            if c:
+                if s[i] == 0:
+                    c += 1
+                elif s[i] == 1:
+                    fragments.append("".join(str(j) for j in s[start:i + c + 1]))
+                    i += c
+                    c = 0
+            else:
+                if s[i] == 1:
+                    fragments.append("1")
+                elif s[i] == 0:
+                    start = i
+                    c += 1
+            i += 1
+
+        result = [self.bin2dec(i) for i in fragments]
+
+        l = []
+
+        for i in result:
+            for i in range(0, i):
+                l.append("1" if prefix else "0")
+            prefix = 0 if prefix else 1
+
+        final = []
+
+        for i in list(self.chunks("".join(l), 8)):
+            final.append(self.dec2rus(self.bin2dec(i)))
+
+        return "".join(final)
+
+    def binStr2bin(self, s, a, b):
+        return "".join(["1" if i == a else "0" for i in s])
+
+
+
+
+a = decoder()
 
 
 
