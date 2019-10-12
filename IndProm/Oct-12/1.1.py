6c4073ddebb5d26d5fdfb1fa6ade168798fa9fcc
= <=>
2019-10-12 10:21:23 +0300

    Oct-12

--- IndProm/Oct-12/1.1.py
+++ IndProm/Oct-12/1.1.py
@@ -1,13 +1,18 @@
 class TraficLight():
     def __init__(self):
-        step = 0
-        values = ['RED', 'YELLOW', 'GREEN']
+        self.step = 0
+        self.values = ['RED', 'YELLOW', 'GREEN']
     def print(self):
-        print('---\n-', "#" if step == 0 else " ", "-\n---\n---\n---\n-",
-            "#" if step == 1 else " ", '-\n---\n---\n---\n-',
-            "#" if step == 2 else " ", '-\n--', sep='')
-    def step(self):
+        print('---\n-', "#" if self.step == 0 else " ", "-\n---\n---\n---\n-",
+            "#" if self.step == 1 else " ", '-\n---\n---\n---\n-',
+            "#" if self.step == 2 else " ", '-\n---', sep='')
+    def step123(self):
         if self.step == 2:
             self.step = 0
         else:
-            step += 1
+            self.step += 1
+
+a = TraficLight()
+a.print()
+a.step123()
+a.print()
