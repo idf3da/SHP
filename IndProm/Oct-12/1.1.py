5de95c19dd647b89fbb9fe18863e8358d2940d6e
= <=>
2019-10-12 10:50:49 +0300

    Oct-12

--- IndProm/Oct-12/1.1.py
+++ IndProm/Oct-12/1.1.py
@@ -1,18 +1,49 @@
 class TraficLight():
     def __init__(self):
-        self.step = 0
+        self.step = 2
         self.values = ['RED', 'YELLOW', 'GREEN']
+        self.value = self.values[self.step]
     def print(self):
-        print('---\n-', "#" if self.step == 0 else " ", "-\n---\n---\n---\n-",
-            "#" if self.step == 1 else " ", '-\n---\n---\n---\n-',
-            "#" if self.step == 2 else " ", '-\n---', sep='')
-    def stepChange(self):
-        if self.step == 2:
-            self.step = 0
+        if self.value != "BLUE":
+            print('###\n###')
+            print('#', '@#' if self.value == "RED" else " #", sep='')
+            print('###')
+            print('#', '@#' if self.value == "YELLOW" else " #", sep='')
+            print('###')
+            print('#', '@#' if self.value == "GREEN" else " #", sep='')
+            print('###\n###')
         else:
+            print("#-#\n" * 9)
+    def stepChange(self):
+        if self.value == "GREEN":
+            self.value = "YELLOW"
+            self.step += 1
+        elif self.value == "YELLOW":
+            if self.step % 2 == 0:
+                self.value = "BLUE"
+                self.BLUE_COUNTER = 0
+            else:
+                self.value = "RED"
+            self.step += 1
+        elif self.value == "BLUE":
+            if self.BLUE_COUNTER < 2:
+                self.BLUE_COUNTER += 1
+            else:
+                self.value = "GREEN"
+                self.step += 1
+        elif self.value == "RED":
+            self.value = "GREEN"
             self.step += 1
 
+
+
+
+
 a = TraficLight()
-a.print()
-a.stepChange()
-a.print()
+
+while True:
+    s = input()
+    if s == "P":
+        a.print()
+    elif s == "S":
+        a.stepChange()
