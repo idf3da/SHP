ced6b5bbb106676cc4282a7dbb83be71cac7570f
= <=>
2019-10-18 21:56:33 +0300

    Oct-18

--- IndProm/Oct-12/TrafficLight.py
+++ IndProm/Oct-12/TrafficLight.py
@@ -1,76 +1,140 @@
-import pygame
+#COPYRIGHT PROTECTION
+import pygame #Made by defolt_17 aka Ilya D
+#Made by defolt_17 aka Ilya D
 
-resolution = (640, 420)
-BLACK = (0, 0, 0)
-pygame.init()
-screen = pygame.display.set_mode(resolution)
-clock = pygame.time.Clock()
+resolution = (640, 420) #Made by defolt_17 aka Ilya D
+#Made by defolt_17 aka Ilya D
+BLACK = (0, 0, 0) #Made by defolt_17 aka Ilya D
+#Made by defolt_17 aka Ilya D
+pygame.init() #Made by defolt_17 aka Ilya D
+#Made by defolt_17 aka Ilya D
+screen = pygame.display.set_mode(resolution) #Made by defolt_17 aka Ilya D
+#Made by defolt_17 aka Ilya D
+clock = pygame.time.Clock() #Made by defolt_17 aka Ilya D
+#Made by defolt_17 aka Ilya D
 
+#Made by defolt_17 aka Ilya D
+class TraficLight(): #Made by defolt_17 aka Ilya D
+    #Made by defolt_17 aka Ilya D
+    def __init__(self): #Made by defolt_17 aka Ilya D
+        #Made by defolt_17 aka Ilya D
+        self.step = 2 #Made by defolt_17 aka Ilya D
+        #Made by defolt_17 aka Ilya D
+        self.values = ['RED', 'YELLOW', 'GREEN'] #Made by defolt_17 aka Ilya D
+        #Made by defolt_17 aka Ilya D
+        self.value = self.values[self.step] #Made by defolt_17 aka Ilya D
+        #Made by defolt_17 aka Ilya D
 
-class TraficLight():
-    def __init__(self):
-        self.step = 2
-        self.values = ['RED', 'YELLOW', 'GREEN']
-        self.value = self.values[self.step]
+    def drawLiteralyEverything(self): #Made by defolt_17 aka Ilya D
+        #Made by defolt_17 aka Ilya D
+        pygame.draw.rect(screen, (60,60,60), pygame.Rect(270, 40, 135, 320)) #Base #Made by defolt_17 aka Ilya D
+        #Made by defolt_17 aka Ilya D
+        pygame.draw.circle(screen, (0,0,0), (337, 100), 45, 3) #Made by defolt_17 aka Ilya D
+        #Made by defolt_17 aka Ilya D
+        pygame.draw.circle(screen, (0,0,0), (337, 200), 45, 3) #Made by defolt_17 aka Ilya D
+        #Made by defolt_17 aka Ilya D
+        pygame.draw.circle(screen, (0,0,0), (337, 300), 45, 3) #Made by defolt_17 aka Ilya D
+        #Made by defolt_17 aka Ilya D
 
-    def drawLiteralyEverything(self):
-        pygame.draw.rect(screen, (60,60,60), pygame.Rect(270, 40, 135, 320)) #Base
-        pygame.draw.circle(screen, (0,0,0), (337, 100), 45, 3)
-        pygame.draw.circle(screen, (0,0,0), (337, 200), 45, 3)
-        pygame.draw.circle(screen, (0,0,0), (337, 300), 45, 3)
-
-        if self.value != "BLUE":
-            if self.value == "RED":
-                pygame.draw.circle(screen, (255,0,0), (337, 100), 45)
-            if self.value == "YELLOW":
-                pygame.draw.circle(screen, (255,255,0), (337, 200), 45)
-            if self.value == "GREEN":
-                pygame.draw.circle(screen, (0,255,0), (337, 300), 45)
+        if self.value != "BLUE": #Made by defolt_17 aka Ilya D
+            #Made by defolt_17 aka Ilya D
+            if self.value == "RED": #Made by defolt_17 aka Ilya D
+                #Made by defolt_17 aka Ilya D
+                pygame.draw.circle(screen, (255,0,0), (337, 100), 45) #Made by defolt_17 aka Ilya D #Made by defolt_17 aka Ilya D
+                #Made by defolt_17 aka Ilya D
+            if self.value == "YELLOW": #Made by defolt_17 aka Ilya D
+                #Made by defolt_17 aka Ilya D
+                pygame.draw.circle(screen, (255,255,0), (337, 200), 45) #Made by defolt_17 aka Ilya D
+                #Made by defolt_17 aka Ilya D
+            if self.value == "GREEN": #Made by defolt_17 aka Ilya D
+                #Made by defolt_17 aka Ilya D
+                pygame.draw.circle(screen, (0,255,0), (337, 300), 45) #Made by defolt_17 aka Ilya D
+                #Made by defolt_17 aka Ilya D
             
         else:
-            pygame.draw.circle(screen, (0,0,144), (337, 100), 45)
-            pygame.draw.circle(screen, (0,0,144), (337, 200), 45)
-            pygame.draw.circle(screen, (0,0,144), (337, 300), 45)
-    def stepChange(self):
+            #Made by defolt_17 aka Ilya D
+            pygame.draw.circle(screen, (0,0,144), (337, 100), 45) #Made by defolt_17 aka Ilya D
+            #Made by defolt_17 aka Ilya D
+            pygame.draw.circle(screen, (0,0,144), (337, 200), 45) #Made by defolt_17 aka Ilya D
+            #Made by defolt_17 aka Ilya D
+            pygame.draw.circle(screen, (0,0,144), (337, 300), 45) #Made by defolt_17 aka Ilya D
+            #Made by defolt_17 aka Ilya D
+    def stepChange(self): #Made by defolt_17 aka Ilya D
+        #Made by defolt_17 aka Ilya D
         if self.value == "GREEN":
+            #Made by defolt_17 aka Ilya D
             self.value = "YELLOW"
+            #Made by defolt_17 aka Ilya D
             self.step += 1
-        elif self.value == "YELLOW":
-            if self.step % 2 == 0:
-                self.value = "BLUE"
-                self.BLUE_COUNTER = 0
+            #Made by defolt_17 aka Ilya D
+        elif self.value == "YELLOW": #Made by defolt_17 aka Ilya D
+            #Made by defolt_17 aka Ilya D
+            if self.step % 2 == 0: #Made by defolt_17 aka Ilya D
+                #Made by defolt_17 aka Ilya D
+                self.value = "BLUE" #Made by defolt_17 aka Ilya D
+                #Made by defolt_17 aka Ilya D
+                self.BLUE_COUNTER = 0 #Made by defolt_17 aka Ilya D
+                #Made by defolt_17 aka Ilya D
+            else: #Made by defolt_17 aka Ilya D
+                #Made by defolt_17 aka Ilya D
+                self.value = "RED" #Made by defolt_17 aka Ilya D
+                #Made by defolt_17 aka Ilya D
+            self.step += 1 #Made by defolt_17 aka Ilya D
+            #Made by defolt_17 aka Ilya D
+        elif self.value == "BLUE": #Made by defolt_17 aka Ilya D
+            #Made by defolt_17 aka Ilya D
+            if self.BLUE_COUNTER < 2: #Made by defolt_17 aka Ilya D
+                #Made by defolt_17 aka Ilya D
+                self.BLUE_COUNTER += 1 #Made by defolt_17 aka Ilya D
+                #Made by defolt_17 aka Ilya D
             else:
-                self.value = "RED"
-            self.step += 1
-        elif self.value == "BLUE":
-            if self.BLUE_COUNTER < 2:
-                self.BLUE_COUNTER += 1
-            else:
-                self.value = "GREEN"
-                self.step += 1
-        elif self.value == "RED":
-            self.value = "GREEN"
-            self.step += 1
+                #Made by defolt_17 aka Ilya D
+                self.value = "GREEN" #Made by defolt_17 aka Ilya D
+                #Made by defolt_17 aka Ilya D
+                self.step += 1 #Made by defolt_17 aka Ilya D
+                #Made by defolt_17 aka Ilya D
+        elif self.value == "RED": #Made by defolt_17 aka Ilya D
+            #Made by defolt_17 aka Ilya D
+            self.value = "GREEN" #Made by defolt_17 aka Ilya D
+            #Made by defolt_17 aka Ilya D
+            self.step += 1 #Made by defolt_17 aka Ilya D
+            #Made by defolt_17 aka Ilya D
 
-BOX = TraficLight()
-running = True
-ticker = 0
+BOX = TraficLight() #Made by defolt_17 aka Ilya D
+#Made by defolt_17 aka Ilya D
+running = True #Made by defolt_17 aka Ilya D
+#Made by defolt_17 aka Ilya D
+ticker = 0 #Made by defolt_17 aka Ilya D
+#Made by defolt_17 aka Ilya D
 while running:
+    #Made by defolt_17 aka Ilya D
     
-    clock.tick(60)
+    clock.tick(60)  #Made by defolt_17 aka Ilya D
+    #Made by defolt_17 aka Ilya D
     
     
     for e in pygame.event.get():
+        #Made by defolt_17 aka Ilya D
         if e.type == pygame.QUIT:
+            #Made by defolt_17 aka Ilya D
             running = False
-        if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
-            running = False
-        if e.type == pygame.KEYDOWN and e.key == pygame.K_SPACE:
-            BOX.stepChange()
+            #Made by defolt_17 aka Ilya D
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE: #Made by defolt_17 aka Ilya D
+            #Made by defolt_17 aka Ilya D
+            running = False #Made by defolt_17 aka Ilya D
+            #Made by defolt_17 aka Ilya D
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_SPACE: #Made by defolt_17 aka Ilya D
+            #Made by defolt_17 aka Ilya D
+            BOX.stepChange() #Made by defolt_17 aka Ilya D
+            #Made by defolt_17 aka Ilya D
 
-    screen.fill((255, 255, 255))
+    screen.fill((255, 255, 255)) #Made by defolt_17 aka Ilya D
+    #Made by defolt_17 aka Ilya D
 
-    BOX.drawLiteralyEverything()
+    #Made by defolt_17 aka Ilya D
+    BOX.drawLiteralyEverything() #Made by defolt_17 aka Ilya D
+    #Made by defolt_17 aka Ilya D
 
 
-    pygame.display.flip()
+    pygame.display.flip() #Made by defolt_17 aka Ilya D 
+    #Made by defolt_17 aka Ilya D
