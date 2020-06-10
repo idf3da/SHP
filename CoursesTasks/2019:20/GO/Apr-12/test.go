de5b4895ad89a4a6a772b9a9de8e3cb00a8c8644
defolt17 <defolt_17@mail.ru>
2020-06-10 13:55:35 +0300

    u know

--- CoursesTasks/2019:20/GO/Apr-12/test.go
+++ CoursesTasks/2019:20/GO/Apr-12/test.go
@@ -1,10 +1,32 @@
 package main
 
-func kek(ch chan int) {
-	ch <- 20
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func fetchSite(url string) (string, error) {
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
 }
+
+func fetchGoogle(q string) (string, error) {
+	return fetchSite("https://www.google.com/search?q=" + q)
+}
+
+func fetchYandex(q string) (string, error) {
+	return fetchSite("https://yandex.ru/search/?text=" + q)
+}
+
 func main() {
-	ch := make(chan int)
-	go kek(ch)
-	print(<-ch)
+	// Используя каналы и селект написать проверку поиска (кто быстрее) на одном запросе.
+	// Желательно провести несколько тестов в цикле
+	res, err := fetchYandex("test")
+	fmt.Println(res, err)
 }
--- CoursesTasks/2019:20/GO/Mar-1/files/md5dir/md5dir.go
+++ CoursesTasks/2019:20/GO/Mar-1/files/md5dir/md5dir.go
@@ -1,28 +1,61 @@
 package main
 
 import (
-    "fmt"
-    "flag"
-    "io/ioutil"
-    "os"
+	"crypto/md5"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"sync"
 )
 
+var (
+	wg = sync.WaitGroup{}
+)
+
+func hash_file_md5(filePath string) {
+	var returnMD5String string
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+	}
+	defer file.Close()
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		fmt.Println("Error occured:", err)
+	}
+	hashInBytes := hash.Sum(nil)[:16]
+	returnMD5String = hex.EncodeToString(hashInBytes)
+
+	fmt.Println(filePath, ":", returnMD5String)
+	wg.Done()
+}
+
 func main() {
-    flag.Parse()
-    dir := "."
-    if flag.Arg(0) != "" {
-        dir = flag.Arg(0)
-    }
-    files, err := ioutil.ReadDir(dir)
-    if err != nil {
-        fmt.Println("Failed to list directory: ", err)
-        os.Exit(1)
-    }
-    for _, file := range files {
-        if file.IsDir() {
-            continue
-        }
-        name := file.Name()
-        fmt.Println(name)
-    }
+
+	flag.Parse()
+	dir := "."
+	if flag.NArg() > 0 {
+		dir = flag.Arg(0)
+	}
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Printf("Failed to read directory: %s", err)
+	}
+
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+
+		go hash_file_md5(path.Join(dir, f.Name()))
+		wg.Add(1)
+
+	}
+
+	wg.Wait()
 }
